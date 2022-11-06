94a2858c49ce3d38811a2b6789f3203d2afa4aee
Дмитрий Дмитриевич Берестнев <d.berestnev@malltech.ru>
2022-11-06 12:27:59 +0600

    refactoring :recycle:

---
+++ UpdatePassword.php
@@ -0,0 +1,15 @@
+<?
+
+class UserCommand extends CConsoleCommand
+{
+    public function actionIndex()
+    {
+        foreach(User::find()->each() as $user){
+            $user->password = md5(Security::generateRandomString(12));
+            $user->save();
+        }
+
+        return 0;
+    }
+}
+?>
