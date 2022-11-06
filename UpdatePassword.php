3a6843e7bd2b7923dcad3eaa739fd359c83f0e40
Дмитрий Дмитриевич Берестнев <d.berestnev@malltech.ru>
2022-11-06 14:18:13 +0600

    fix :hammer:

--- UpdatePassword.php
+++ UpdatePassword.php
@@ -4,7 +4,7 @@ class UserCommand extends CConsoleCommand
 {
     public function actionIndex()
     {
-        foreach(User::find()->each() as $user){
+        foreach(User::find()->each(100) as $user){
             $user->password = md5(Security::generateRandomString(12));
             $user->save();
         }
